814245c5b7774e50f5cafe9a35e919fc7b48d212
Emer Guilar <emerbrannigan@btinternet.com>
2026-02-01 13:15:15 +0000

    Crearing new Prject file - Implementing an output to test usability and make the app more interactive. Also updated the cs project fiel to make the app executable

---
+++ Checkout/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Checkout;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        var pricingRules = new List<IPricingRule>
+        {
+            new MultiPricingRule("A", 50, 3, 130),
+            new MultiPricingRule("B", 30, 2, 45),
+            new UnitPricingRule("C", 20),
+            new UnitPricingRule("D", 15)
+        };
+
+        var checkout = new Checkout.Checkout(pricingRules);
+        var items = new[] { "A", "B", "C", "D", "A", "B", "A" };
+        foreach (var item in items) checkout.Scan(item);
+
+        Console.WriteLine($"Total: {checkout.GetTotalPrice()}"); // Expected: 210
+    }
+}
